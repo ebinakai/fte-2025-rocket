3c02411abc6a51ba12a768f0d5225e189cc898f0
EbinaKai <blackbicycle55@gmail.com>
2025-08-10 13:48:12 +0900

    update: LEDの点灯を追加

--- main.py
+++ main.py
@@ -4,19 +4,26 @@ import board
 import csv
 import threading
 import collections
-from datetime import datetime
+from datetime import datetime, timedelta
 from adafruit_bme280 import basic as adafruit_bme280  # noqa: E402
 from adafruit_bno055 import BNO055_I2C  # noqa: E402
+from pathlib import Path
 
 # 実行時間
-RUN_TIME = 10  # seconds
+RUN_TIME = 10         # sec
+FLUSH_INTERVAL = 0.5  # sec
 
 # I2C定義
 SDA_PIN = 2
 SCL_PIN = 3
 I2C_BUS_ID = 1
-BME280_I2C_ADDR = 0x76
+BME280_I2C_ADDR = 0x77
 BNO055_I2C_ADDR = 0x28
+
+# GPIOピン定義
+LED_PIN = 6
+PWM_PIN = 18
+INTRPT_PIN = 17
 
 class SensorReader:
     def __init__(self, pi, freq=100, output_file="sensor_log.csv", flush_interval=1.0):
@@ -25,7 +32,6 @@ class SensorReader:
         self.tick = 0
         self.output_file = output_file
         self.flush_interval = flush_interval
-        self.running = True
 
         self.buffer = collections.deque()
         self.lock = threading.Lock()
@@ -49,22 +55,21 @@ class SensorReader:
         # センサ読み取りスレッド起動
         self.bme_thread = threading.Thread(target=self.read_bme280_loop)
         self.bno_thread = threading.Thread(target=self.read_bno055_loop)
-        self.bme_thread.start()
-        self.bno_thread.start()
+        
+        # LED点灯スレッド
+        self.led_thread = threading.Thread(target=self.flush_led)
 
         # GPIO設定とPWM開始
-        self.output_pin = 18
-        self.input_pin = 17
+        self.output_pin = PWM_PIN
+        self.input_pin = INTRPT_PIN
         self.pi.set_mode(self.output_pin, pigpio.OUTPUT)
         self.pi.set_mode(self.input_pin, pigpio.INPUT)
-        self.pi.hardware_PWM(self.output_pin, freq, 500000)
 
         # 割込み登録
         self.cb = self.pi.callback(self.input_pin, pigpio.RISING_EDGE, self.read_sensors)
 
-        # 書き出しスレッド起動
+        # 書き出しスレッド
         self.writer_thread = threading.Thread(target=self.flush_to_csv_loop)
-        self.writer_thread.start()
 
         # ヘッダ書き込み
         with open(self.output_file, "w", newline="") as f:
@@ -131,6 +136,7 @@ class SensorReader:
 
     def flush_to_csv_loop(self):
         while self.running:
+            # フラッシュ間隔を待機
             time.sleep(self.flush_interval)
             with self.lock:
                 rows = list(self.buffer)
@@ -140,6 +146,27 @@ class SensorReader:
                     writer = csv.writer(f)
                     writer.writerows(rows)
 
+    def flush_led(self):
+        cur_led_state = False
+        while self.running:
+            # フラッシュ間隔を待機
+            time.sleep(self.flush_interval / 2)
+
+            # LEDの状態をトグル
+            if cur_led_state:
+                self.pi.write(LED_PIN, pigpio.LOW)  # LED OFF
+            else:
+                self.pi.write(LED_PIN, pigpio.HIGH)  # LED ON
+            cur_led_state = not cur_led_state
+
+    def start(self):
+        self.running = True
+        self.pi.hardware_PWM(self.output_pin, self.freq, 500000)
+        self.writer_thread.start()
+        self.bme_thread.start()
+        self.bno_thread.start()
+        self.led_thread.start()
+
     def stop(self):
         self.running = False
         self.cb.cancel()
@@ -147,7 +174,7 @@ class SensorReader:
         self.writer_thread.join()
         self.bme_thread.join()
         self.bno_thread.join()
-
+        self.led_thread.join()
 
 if __name__ == "__main__":
     pi = pigpio.pi()
@@ -158,13 +185,22 @@ if __name__ == "__main__":
     # 現在時刻を使ってファイル名を生成
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     output_filename = f"sensor_log_{timestamp}.csv"
-    
-    reader = SensorReader(pi, freq=100, output_file=output_filename)
+    output_file = Path('results')
+    output_file.mkdir(parents=True, exist_ok=True)
+    # output_file = output_file / output_filename
+    output_file = output_file / 'test.csv'
+
+    reader = SensorReader(pi, freq=100, output_file=output_file, flush_interval=FLUSH_INTERVAL)
+    pi.set_mode(LED_PIN, pigpio.OUTPUT)
     try:
-        print(f"記録を開始しました（ファイル: {output_filename}）")
+        # LED点灯
+        reader.start()
+        print(f"記録を開始しました（ファイル: {output_file}）")
         time.sleep(RUN_TIME)
     except KeyboardInterrupt:
         print("終了処理中...")
     finally:
         reader.stop()
+        pi.write(LED_PIN, pigpio.LOW)  # LED OFF
         pi.stop()
+        print("記録を停止しました")
